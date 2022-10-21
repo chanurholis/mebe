b7956c77726a26e6182d9b5b3d3ad7c138ba0555
Chacha Nurholis <chachanurholis29@gmail.com>
2022-10-22 03:12:26 +0700

    feat: add landing page container

--- app/containers/public/LandingPage/index.tsx
+++ app/containers/public/LandingPage/index.tsx
@@ -1,12 +1,14 @@
-import { Button } from 'antd';
+import MainLayout from "@/layouts/MainLayout";
 
-function LandingPage() {
+const LandingPage = () => {
   return (
-    <div>
-      <Button>Oke</Button>
-      <Button type='primary'>Primary Button</Button>
-    </div>
-  );
+    <MainLayout>
+      <div>
+        <h1>Landing Page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta dolorum aspernatur incidunt nisi eos! Voluptates dolores quas natus amet corporis ratione iure. Delectus molestias ad, deleniti nisi iste quisquam quas consequatur animi. Esse laudantium quis, accusantium quidem rerum eveniet molestiae? Quisquam, voluptate aperiam! Tenetur temporibus vero necessitatibus, corrupti quas eius blanditiis quibusdam sunt aut, voluptatem magni dolorem dolore facilis quae fugiat eos labore odit nemo, consectetur repudiandae? Sit consequatur dolorem nam voluptas aperiam cumque cum ullam sint aliquam aut. Possimus quaerat illo corrupti illum error corporis alias nobis magnam ea repellat harum earum omnis reprehenderit itaque, odit beatae labore dolore.</p>
+      </div>
+    </MainLayout>
+  )
 }
 
 export default LandingPage;
