d75a2d2ff5caa397743e8cde0018d2b2c5dacdad
Chacha Nurholis <chachanurholis29@gmail.com>
2022-10-22 21:53:52 +0700

    improve: example styled

--- app/containers/public/LandingPage/index.tsx
+++ app/containers/public/LandingPage/index.tsx
@@ -1,12 +1,20 @@
+import styled from "@emotion/styled";
+
 import MainLayout from "@/layouts/MainLayout";
+import Button from "@/components/Button";
+
+const StyledButton = styled(Button)`
+  padding: 32px
+`
 
 const LandingPage = () => {
   return (
     <MainLayout>
-      <div>
+      <>
         <h1>Landing Page</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta dolorum aspernatur incidunt nisi eos! Voluptates dolores quas natus amet corporis ratione iure. Delectus molestias ad, deleniti nisi iste quisquam quas consequatur animi. Esse laudantium quis, accusantium quidem rerum eveniet molestiae? Quisquam, voluptate aperiam! Tenetur temporibus vero necessitatibus, corrupti quas eius blanditiis quibusdam sunt aut, voluptatem magni dolorem dolore facilis quae fugiat eos labore odit nemo, consectetur repudiandae? Sit consequatur dolorem nam voluptas aperiam cumque cum ullam sint aliquam aut. Possimus quaerat illo corrupti illum error corporis alias nobis magnam ea repellat harum earum omnis reprehenderit itaque, odit beatae labore dolore.</p>
-      </div>
+        <StyledButton type="primary">ass</StyledButton>
+      </>
     </MainLayout>
   )
 }
