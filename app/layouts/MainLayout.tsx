4d9762af9364091992bf0b753b0d9e97029e0b1c
Chacha Nurholis <chachanurholis29@gmail.com>
2022-10-22 22:00:49 +0700

    improve: main layout

--- app/layouts/MainLayout.tsx
+++ app/layouts/MainLayout.tsx
@@ -1,43 +1,13 @@
-import { FC, createElement, ReactElement } from 'react';
-
-import { Layout } from 'antd';
-import type { MenuProps } from 'antd';
-import { DashboardOutlined, ThunderboltOutlined } from '@ant-design/icons';
-
-import SiderLayout from '@/layouts/SiderLayout';
-import FooterLayout from '@/layouts/FooterLayout';
-import HeaderLayout from '@/layouts/HeaderLayout';
-
-const { Content } = Layout;
-
-const items: MenuProps['items'] = [
-  {
-    key: String(1),
-    icon: createElement(DashboardOutlined),
-    label: 'Dashboard',
-  },
-  {
-    key: String(2),
-    icon: createElement(ThunderboltOutlined),
-    label: 'World Cup',
-  },
-];
+import { FC, ReactNode } from "react"
 
 interface IProps {
-  children: ReactElement;
+  children: ReactNode;
 }
 
-const MainLayout: FC<IProps> = ({ children }) => (
-  <Layout hasSider>
-    <SiderLayout items={items} />
-    <Layout className="site-layout" style={{ marginLeft: 200 }}>
-      <HeaderLayout />
-      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-        {children}
-      </Content>
-      <FooterLayout label='Ant Design ©2018 Created by Ant UED' />
-    </Layout>
-  </Layout>
-);
+const MainLayout: FC<IProps> = ({ children }) => {
+  return (
+    <>{children}</>
+  )
+}
 
 export default MainLayout;
